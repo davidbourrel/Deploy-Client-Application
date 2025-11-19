7943690940ea707cdc1a1582fdc6be48ba02f5c4
davidbourrel <david-bourrel@hotmail.fr>
2025-11-19 21:28:16 +0100

    step 1 of workspace-accounts-creation done

--- src/app/feature/client-deployment/client-deployment.ts
+++ src/app/feature/client-deployment/client-deployment.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
+import { Headings } from '../../shared/components/headings/headings';
 import { DeploymentSteps } from './deployment-steps/deployment-steps';
-import { Headings } from "../../shared/components/headings/headings";
 
 @Component({
   selector: 'app-client-deployment',
   imports: [DeploymentSteps, TranslocoModule, Headings],
   templateUrl: './client-deployment.html',
 })
-export class ClientDeployment {}
+export class ClientDeployment {
+  protected readonly clientName = signal('');
+
+  onClientNameChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.clientName.set(input.value);
+    console.log('Nom du client:', this.clientName());
+  }
+}
